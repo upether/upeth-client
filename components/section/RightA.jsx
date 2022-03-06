3b7eb4a610adeb08aca7c63714bbe0e3fb5172da
royalbluee <tkyeom96@gmail.com>
2022-03-07 03:15:01 +0900

    feat: Add RightA

--- components/section/RightA.jsx
+++ components/section/RightA.jsx
@@ -1,9 +1,14 @@
 import styles from '../../styles/RightA.module.css';
+import OrderA from '../order/OrderA';
+import TabA from '../tab/TabA';
 
 function RightA() {
   return (
     <div className={styles.rightA}>
-      <article>Right</article>
+      <article>
+        <TabA />
+        <OrderA />
+      </article>
     </div>
   );
 }
