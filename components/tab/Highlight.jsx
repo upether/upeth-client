7bd383d4f0290737dd34433db75c0f7fd907014a
royalbluee <tkyeom96@gmail.com>
2022-03-11 16:24:31 +0900

    feat: Add selectOption handler

--- components/tab/Highlight.jsx
+++ components/tab/Highlight.jsx
@@ -1,6 +1,26 @@
+import React, { useState, useCallback } from 'react';
 import { Block } from './Highlight.styles';
 
+const Image = React.memo(({ idx, hlOption }) => {
+  const source =
+    hlOption[0] === parseInt(idx)
+      ? hlOption[1]
+        ? 'https://cdn.upbit.com/images/ico_up_down_2.71770c7.png'
+        : 'https://cdn.upbit.com/images/ico_up_down_1.d63eb3d.png'
+      : 'https://cdn.upbit.com/images/ico_up_down.d050377.png';
+
+  return <img src={source} />;
+});
+
 function Highlight() {
+  const [korName, setKorName] = useState(true);
+  const [hlOption, setHlOption] = useState([3, true]);
+
+  const selectHlOption = useCallback((idx) => {
+    if (idx === hlOption[0]) setHlOption([idx, !hlOption[1]]);
+    else setHlOption([idx, true]);
+  });
+
   return (
     <Block>
       <colgroup>
@@ -14,27 +34,27 @@ function Highlight() {
       <thead>
         <tr>
           <th colSpan="3">
-            <a href="#">
-              한글명
+            <a href="#" onClick={() => setKorName((prevState) => !prevState)}>
+              {korName ? '한글명' : '영문명'}
               <img src="https://cdn.upbit.com/images/ico_change.c6ad0e9.png" />
             </a>
           </th>
           <th>
-            <a href="#">
+            <a href="#" onClick={() => selectHlOption(1)}>
               현재가
-              <img src="https://cdn.upbit.com/images/ico_up_down.d050377.png" />
+              <Image idx="1" hlOption={hlOption} />
             </a>
           </th>
           <th>
-            <a href="#">
+            <a href="#" onClick={() => selectHlOption(2)}>
               전일대비
-              <img src="https://cdn.upbit.com/images/ico_up_down.d050377.png" />
+              <Image idx="2" hlOption={hlOption} />
             </a>
           </th>
           <th>
-            <a href="#">
+            <a href="#" onClick={() => selectHlOption(3)}>
               거래대금
-              <img src="https://cdn.upbit.com/images/ico_up_down_2.71770c7.png" />
+              <Image idx="3" hlOption={hlOption} />
             </a>
           </th>
         </tr>
