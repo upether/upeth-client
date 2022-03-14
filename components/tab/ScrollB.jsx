b38fecdbc5d567d15f65e74d52574207262aa4fe
royalbluee <tkyeom96@gmail.com>
2022-03-14 23:06:26 +0900

    feat: Set marketData format

--- components/tab/ScrollB.jsx
+++ components/tab/ScrollB.jsx
@@ -4,27 +4,77 @@ import { Scrollbars } from 'react-custom-scrollbars';
 import { Block } from './ScrollB.styles';
 
 import TableB from '../table/TableB';
+
 function ScrollB() {
   const { isLoading, error, data } = useQuery('marketData', () =>
     fetch('https://api.upbit.com/v1/market/all').then((res) => res.json())
   );
 
   let marketData;
+  let symbolData;
+  let totalData;
+  let totalCoinData;
 
-  if (isLoading) return 'Loading...';
-
-  if (error) return 'An error has occurred: ' + error.message;
-
-  marketData = data.filter((el) => {
+  marketData = data?.filter((el) => {
     let [a, b] = el.market.split('-');
     if (a === 'KRW') return el;
   });
 
-  console.log('scrollB');
-  console.log('data');
-  console.log(data);
-  console.log('marketData');
-  console.log(marketData);
+  symbolData = marketData?.map((el) => el.market);
+  totalData = symbolData?.join(',');
+
+  const { data: tickerTotalData } = useQuery(
+    ['tickerTotalData', totalData],
+    () =>
+      fetch(`https://api.upbit.com/v1/ticker?markets=${totalData}`).then(
+        (res) => res.json()
+      )
+  );
+
+  console.log(tickerTotalData);
+
+  totalCoinData = tickerTotalData?.map((el, i) => {
+    const {
+      market,
+      change,
+      trade_price,
+      change_rate,
+      change_price,
+      acc_trade_price_24h,
+      high_price,
+      low_price,
+    } = el;
+    const { korean_name } = marketData[i];
+    return {
+      market,
+      change,
+      trade_price,
+      change_rate,
+      change_price,
+      acc_trade_price_24h,
+      high_price,
+      low_price,
+      korean_name,
+    };
+  });
+
+  console.log(totalCoinData);
+
+  // 정렬
+  // totalCoinData.sort((a, b) => b.acc_trade_price_24h - a.acc_trade_price_24h);
+
+  // market: KRW-BTC
+  // change: 'RISE', 'FALL', 'EVEN'
+  // 현재가 - trade_price
+  // 전일대비 퍼센트 - change_rate
+  // 전일대비 가격차 - change_price
+  // 거래대금 - acc_trade_price_24h
+
+  // 캔들 - high_price, low_price
+
+  if (isLoading) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
 
   return (
     <Block>
@@ -34,8 +84,11 @@ function ScrollB() {
         .map((el, i) => (
           <TableB key={i} />
         ))} */}
-        {marketData.map((el, i) => (
+        {/* {marketData?.map((el, i) => (
           <TableB key={i} name={el.korean_name} market={el.market} />
+        ))} */}
+        {totalCoinData?.map((el, i) => (
+          <TableB key={i} coinData={el} />
         ))}
       </Scrollbars>
     </Block>
--- components/table/TableB.jsx
+++ components/table/TableB.jsx
@@ -11,13 +11,24 @@ import {
   Volume,
 } from './TableB.styles';
 
-function TableB({ name, market }) {
+function TableB({ coinData }) {
+  const {
+    market,
+    change,
+    trade_price,
+    change_rate,
+    change_price,
+    acc_trade_price_24h,
+    high_price,
+    low_price,
+    korean_name,
+  } = coinData;
   let [a, b] = market.split('-');
 
   console.log('TableB');
 
   return (
-    <Block>
+    <Block change={change}>
       <colgroup>
         <col width="26" />
         <col width="26" />
@@ -27,7 +38,9 @@ function TableB({ name, market }) {
         <col width="*" />
       </colgroup>
       <tbody>
-        <tr className="up">
+        {/* {change === 'RISE' ? <tr className="up"> : <tr className="down">} */}
+        {/* <tr className="up"> */}
+        <tr>
           <td>
             <Bookmark>
               <a href="#">즐겨찾기</a>
@@ -43,7 +56,7 @@ function TableB({ name, market }) {
           </Candle>
           <Title>
             <a href="#">
-              <strong>{name}</strong>
+              <strong>{korean_name}</strong>
             </a>
             <em>
               {b}
@@ -51,16 +64,46 @@ function TableB({ name, market }) {
             </em>
           </Title>
           <Price>
-            <strong>22,450</strong>
+            <strong>
+              {trade_price > 100
+                ? trade_price
+                    .toString()
+                    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+                : trade_price >= 1
+                ? trade_price.toFixed(2)
+                : trade_price.toFixed(4)}
+            </strong>
             <span></span>
           </Price>
           <Percent>
-            <p>+11.89%</p>
-            <em>2,410</em>
+            {/* <p>+11.89%</p>
+            <em>2,410</em> */}
+            {change === 'RISE' ? (
+              <p>{'+' + (change_rate * 100).toFixed(2) + '%'}</p>
+            ) : change === 'FALL' ? (
+              <p>{'-' + (change_rate * 100).toFixed(2) + '%'}</p>
+            ) : (
+              <p>{(change_rate * 100).toFixed(2) + '%'}</p>
+            )}
+            <em>
+              {change_price > 100
+                ? change_price
+                    .toString()
+                    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+                : change_price >= 1
+                ? change_price.toFixed(2)
+                : change_price.toFixed(4)}
+            </em>
           </Percent>
           <Volume>
-            <p>
+            {/* <p>
               957,471<i>백만</i>
+            </p> */}
+            <p>
+              {Math.floor(acc_trade_price_24h / 1000000)
+                .toString()
+                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+              <i>백만</i>
             </p>
           </Volume>
         </tr>
