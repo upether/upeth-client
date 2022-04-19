28414e212a69198a974f5cd15d5a23cbc39b5166
royalbluee <tkyeom96@gmail.com>
2022-04-20 00:11:06 +0900

    feat: Add setDataFormat in utils Folder

--- components/articleB/CoinListContainerB.jsx
+++ components/articleB/CoinListContainerB.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { observer } from 'mobx-react';
+import { Scrollbars } from 'react-custom-scrollbars';
+import { Block } from './styles/CoinListContainer.styles';
 
+import CoinListItem from './CoinListItem';
+import CoinListEmptyText from './CoinListEmptyText';
 import useExchange from '../../hooks/useExchange';
 import useMarketAll from './hooks/useMarketAll';
 import useTickerAll from './hooks/useTickerAll';
+import useTickerBookmark from '../../hooks/useTickerBookmark';
 
 const CoinListContainerB = observer(() => {
   const exchangeStore = useExchange();
@@ -18,7 +23,61 @@ const CoinListContainerB = observer(() => {
   );
   console.log(tickerAllData);
 
-  return <div></div>;
+  const setBookmark = useCallback((market) => {
+    const bookmark = localStorage.getItem('bookmark');
+    if (bookmark) {
+      const parseBookmark = JSON.parse(bookmark);
+      if (!parseBookmark.includes(market)) {
+        localStorage.setItem(
+          'bookmark',
+          JSON.stringify([...parseBookmark, market])
+        );
+      } else {
+        localStorage.setItem(
+          'bookmark',
+          JSON.stringify(parseBookmark.filter((el) => el !== market))
+        );
+      }
+    } else {
+      localStorage.setItem('bookmark', JSON.stringify([market]));
+    }
+  }, []);
+
+  let bookmarkData;
+
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('bookmark') === null) {
+      localStorage.setItem('bookmark', '[]');
+    } else {
+      const { marketAllData } = useMarketAll();
+      const { totalCoinData: bookmark } = useTickerBookmark(
+        marketAllData,
+        JSON.parse(localStorage.getItem('bookmark')).join(),
+        exchangeStore.headerOption,
+        exchangeStore.searchInput
+      );
+      bookmarkData = bookmark;
+    }
+  }
+
+  return (
+    <Block>
+      <Scrollbars style={{ width: '100%', height: '770px' }} universal={true}>
+        {exchangeStore.optionB !== '보유' &&
+        exchangeStore.optionB !== '관심' ? (
+          tickerAllData?.map((el, i) => (
+            <CoinListItem key={i} coinData={el} setBookmark={setBookmark} />
+          ))
+        ) : exchangeStore.optionB === '보유' ? (
+          <CoinListEmptyText />
+        ) : (
+          bookmarkData?.map((el, i) => (
+            <CoinListItem key={i} coinData={el} setBookmark={setBookmark} />
+          ))
+        )}
+      </Scrollbars>
+    </Block>
+  );
 });
 
 export default CoinListContainerB;
---
+++ utils/setDataFormat.js
@@ -0,0 +1,26 @@
+export const setPriceFormat = (price) => {
+  if (price >= 100) {
+    return price.toLocaleString('ko-KR');
+  } else if (price >= 1) {
+    return price.toFixed(2);
+  } else {
+    return price.toFixed(4);
+  }
+};
+
+export const setVolumeFormat = (price, volume) => {
+  if (price >= 1) {
+    return volume
+      .toFixed(3)
+      .toString()
+      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  } else {
+    return Math.floor()
+      .toString()
+      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  }
+};
+
+export const setDateFormat = (date) => {
+  return date.replace(/-/g, '.');
+};
